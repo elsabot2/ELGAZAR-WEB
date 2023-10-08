cf0ddd044604a83a8e11afad56a8b8f514a0c54a
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-10-08 21:39:40 +0700

    adding loader

--- src/App.js
+++ src/App.js
@@ -1,40 +1,67 @@
+import React, { useEffect, useState } from "react";
 import Animation from "./components/Animation/Animation";
 import Footer from "./components/Footer";
 import Links from "./components/Links";
 import PhotoProfile from "./components/PhotoProfile";
+import Loader from "./components/Loader";
 
 const App = () => {
+  const [loading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 6000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="
-    flex
-    flex-col
-    items-center
-    justify-center
-    min-h-screen
-    ">
-      <Animation/>
-      <PhotoProfile/>
-      <div>
-        <h1 className="
-        sm:text-3xl 
-        md:text-4xl
-        text-3xl
-        font-bold
-        text-center
-        text-gray-300
-        pt-6 
-        sm:pt-6
-        ">Ahmad Ammar Musyaffa</h1>
-        <p className="
-        text-gray-500
-        text-center
-        pb-10
-        ">Junior Web Developer</p>
-      </div>
-      <Links />
-      <Footer />
+    <div
+      className="
+      flex
+      flex-col
+      items-center
+      justify-center
+      min-h-screen
+      "
+    >
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Animation />
+          <PhotoProfile />
+          <div>
+            <h1
+              className="
+          sm:text-3xl 
+          md:text-4xl
+          text-3xl
+          font-bold
+          text-center
+          text-gray-300
+          pt-6 
+          sm:pt-6
+          "
+            >
+              Ahmad Ammar Musyaffa
+            </h1>
+            <p
+              className="
+          text-gray-500
+          text-center
+          pb-10
+          "
+            >
+              Junior Web Developer
+            </p>
+          </div>
+          <Links />
+          <Footer />
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Loader.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div>
+      <div
+        className="
+        flex
+        flex-col
+        items-center
+        justify-center
+        min-h-screen
+        "
+      >
+        <svg
+          className="animate-spin h-16 w-16 text-teal-500 mx-auto"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2.645-2.645a9.963 9.963 0 012.37-2.37M17.355 2.645a9.963 9.963 0 012.37 2.37"
+          ></path>
+        </svg>
+        <h2 className="text-center text-2xl text-white font-bold pt-4
+        sm:text-3xl
+        md:text-4xl
+        ">
+          Loading...
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
